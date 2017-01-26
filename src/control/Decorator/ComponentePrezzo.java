c7b1bf3accbc0558541afe5228f18dd3a4f38a92
majoc <catullomary.jo@libero.it>
2017-01-26 18:03:52 +0100

    Activity & UC Maria, modifiche specifica dei requisiti, matrice di tracciabilità

--- src/control/Decorator/ComponentePrezzo.java
+++ src/control/Decorator/ComponentePrezzo.java
@@ -3,7 +3,7 @@ package control.Decorator;
 /**
  * Created by maria on 13/01/17.
  */
-//component
+//Component
 public abstract class ComponentePrezzo {
     public abstract int calcolaPrezzo();
 }
